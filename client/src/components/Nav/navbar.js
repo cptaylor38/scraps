66d09b7d76baed93b0c057c5ae469835767dd6b0
Corbin Taylor <cptaylor38@yahoo.com>
2019-06-30 12:23:10 -0400

    attempting to fix navbar

--- client/src/components/Nav/navbar.js
+++ client/src/components/Nav/navbar.js
@@ -4,15 +4,17 @@ import './navbar.css';
 const Navbar = ({ user, onClick }) => {
 
     return (
-        <nav className="navbar container-fluid">
-            <div className='logoHolder'>
-                <h1>SCRAPS</h1>
-            </div>
-            <div className='messageHolder'>
-                <h3>Welcome, {user ? user : ''} </h3>
-            </div>
-            <div className='faceHolder'>
-                <img src='https://slack-imgs.com/?c=1&url=https%3A%2F%2Fthumbs.gfycat.com%2FOrdinaryPartialAmericanredsquirrel-size_restricted.gif' alt='monkey gif' id='facePic'></img>
+        <nav className="navbar">
+            <div className='navContainer'>
+                <div className='logoHolder'>
+                    <h1>SCRAPS</h1>
+                </div>
+                <div className='messageHolder'>
+                    <h3>Welcome, {user ? user : ''} </h3>
+                </div>
+                <div className='faceHolder'>
+                    <img src='https://slack-imgs.com/?c=1&url=https%3A%2F%2Fthumbs.gfycat.com%2FOrdinaryPartialAmericanredsquirrel-size_restricted.gif' alt='monkey gif' id='facePic'></img>
+                </div>
             </div>
         </nav>
     );
