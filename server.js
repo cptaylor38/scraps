ec454af77d4747e9e0d2cc25891e6cd9495a6744
Corbin Taylor <cptaylor38@yahoo.com>
2019-06-30 02:40:30 -0400

    still trying to deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +11,11 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use('/static', express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+
 }
 // Add routes, both API and view
 app.use("/", routes);
